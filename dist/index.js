76de6fb9059fb43b282afb5ce2128b8bb6d4dd2c
Dennis Ilyeechev <dennisilyeechev@Moveos-MacBook-Pro-3.local>
2022-04-11 16:40:07 +0300

    fixed

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let num = 5;
+console.log('hello');
+function getData() {
+}
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+let num = 5
+console.log('hello');
+
+
+function getData() {
+    
+}
